d58771fff67f24c4126b15bad1df0a98bfee48d6
mareq022 <mareq022@gmail.com>
2020-06-15 21:17:11 +0200

    Moduł ObjectLoader

---
+++ Assets/Scripts/ObjectLoader.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectLoader : MonoBehaviour
+{
+    public string[] filePaths;
+
+    public int currentModelIndex;
+    public int startingIndex = 0;
+
+    public Object currentModel;
+
+    public string modelType = "fbx"; //To get prefabs change to "prefab"
+
+    private static string pathIn = "Assets/In/";
+    private static string pathOut = "Assets/Out";
+
+
+    void Start()
+    {
+        currentModelIndex = startingIndex;
+
+        filePaths = Directory.GetFiles(pathIn, "*." + modelType + " ");
+
+        LoadModel();
+    }
+
+    public void Next()
+    {
+        if(currentModelIndex == filePaths.Length)
+        {
+            currentModelIndex = 0;
+        }else
+        {
+            currentModelIndex += 1;
+        }
+        LoadModel();
+    }
+
+    public void Back()
+    {
+        if(currentModelIndex == 0)
+        {
+            currentModelIndex = filePaths.Length;
+        }else
+        {
+            currentModelIndex -= 1;
+        }
+        LoadModel();
+    }
+
+    public void LoadModel()
+    {
+        currentModel = AssetDatabase.LoadMainAssetAtPath(filePaths[currentModelIndex]);
+        if (currentModel != null) Instantiate(currentModel);
+    }
+}
