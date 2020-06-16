c78e40ec957c2ae497c4547ae22dad8647e0100b
mareq022 <mareq022@gmail.com>
2020-06-16 15:30:44 +0200

    CameraCapture - moduł odpowiedzialny za robienie zdjęć

---
+++ Assets/Scripts/CameraCapture.cs
@@ -0,0 +1,32 @@
+﻿ 
+using System.IO;
+using UnityEngine;
+ 
+public class CameraCapture : MonoBehaviour
+{
+    private Camera cam;
+    private int i=1;
+ 
+    void Start () {
+        cam = GetComponent<Camera>();
+    }
+ 
+    public void Capture()
+    {
+        RenderTexture activeRenderTexture = RenderTexture.active;
+        RenderTexture.active = cam.targetTexture;
+
+        cam.Render();
+ 
+        Texture2D image = new Texture2D(cam.targetTexture.width, cam.targetTexture.height);
+        image.ReadPixels(new Rect(0, 0, cam.targetTexture.width, cam.targetTexture.height), 0, 0);
+        image.Apply();
+        RenderTexture.active = activeRenderTexture;
+ 
+        byte[] bytes = image.EncodeToPNG();
+        Destroy(image);
+ 
+        File.WriteAllBytes("Assets/Out/photo" + i, bytes);
+        i++;
+    }
+}
